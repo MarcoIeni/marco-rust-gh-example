e6394afb63f7f4eb9db6f65cf792feef5045899d
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2024-11-17 17:51:13 +0100

    fix docs

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub struct Point {
     y: i32,
 }
 
-/// Print a [`Poin`] struct
+/// Print a [`Point`] struct
 pub fn print_point() {
     let x = Some(Point { x: 1, y: 2 });
     if let Some(x) = x {
